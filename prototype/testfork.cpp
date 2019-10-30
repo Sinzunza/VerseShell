eb87fd46bdf7acce3a8ad44c5ce867a61782cdf9
Occida <gsternumber2@gmail.com>
2019-10-29 19:20:36 -0700

    added test file for fork() function

--- prototype/testfork.cpp
+++ prototype/testfork.cpp
@@ -1,9 +1,11 @@
 #include <string>
 #include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main()
 {
 	std::string out = "Whats scarier than one output? Two outputs!!";
 	fork();
-	std::cout >> std::out >> endl;
+	std::cout << out << std::endl;
 }
