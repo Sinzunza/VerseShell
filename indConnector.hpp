3fd630378e0441b58f332b05b963b2ef3afaeba3
Sergio Inzunza <sergioinz72@live.com>
2019-11-04 16:54:40 -0800

    created indConnector and orConnector class

---
+++ indConnector.hpp
@@ -0,0 +1,29 @@
+#ifndef INDCONNECTOR_HPP
+#define INDCONNECTOR_HPP
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "base.hpp"
+
+class indConnector : public base {
+	public:
+		indConnector() : base() { succeeded = false; }
+        	virtual void execute(char* args[]){
+            		pid_t pid = fork();
+            		if (pid == 0){
+                		execvp(args[0],args);
+            		}
+            		else {
+				int status;
+            			waitpid(0, &status, WCONTINUED);
+				if(WIFEXITED(status)){
+					succeeded = true;
+				}	     
+	    		}
+		}
+		
+};
+
+#endif
+
