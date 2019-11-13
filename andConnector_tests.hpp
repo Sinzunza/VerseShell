caed0a7df42a8d11f7a5cd3938ce78db523fbc4f
Sergio Inzunza <sergioinz72@live.com>
2019-11-13 02:21:32 -0800

    created unit test for all connector classes

---
+++ andConnector_tests.hpp
@@ -0,0 +1,116 @@
+#ifndef __ANDCONNECTOR_TESTS_HPP__
+#define __ANDCONNECTOR_TESTS_HPP__
+
+#include "gtest/gtest.h"
+
+#include "base.hpp"
+#include "indConnector.hpp"
+#include "orConnector.hpp"
+#include "andConnector.hpp"
+
+TEST(andConnectorTests, using_echo_succeeded) {
+    std::string cmdOneOr = "echo";
+    char *charOneOr = const_cast<char*>(cmdOneOr.c_str());
+    std::string argOneOr = "Hello World";
+    char *charTwoOr = const_cast<char*>(argOneOr.c_str());
+    char *charArOr[3] = {charOneOr,charTwoOr,NULL};
+    base *indBase = new indConnector(charArOr);
+    indBase->execute();
+
+    std::string cmdOne = "echo";
+    char *charOne = const_cast<char*>(cmdOne.c_str());
+    std::string argOne = "Hello World Two";
+    char *charTwo = const_cast<char*>(argOne.c_str());
+    char *charAr[3] = {charOne,charTwo,NULL};
+    base *andBase = new andConnector(indBase, charAr);
+    andBase->execute();
+
+    EXPECT_EQ(andBase->getSucceeded(), true);
+}
+
+TEST(andConnectorTests, using_echo_not_succeeded) {
+    std::string cmdOneOr = "fdgjk";
+    char *charOneOr = const_cast<char*>(cmdOneOr.c_str());
+    std::string argOneOr = "Hello World";
+    char *charTwoOr = const_cast<char*>(argOneOr.c_str());
+    char *charArOr[3] = {charOneOr,charTwoOr,NULL};
+    base *indBase2 = new indConnector(charArOr);
+    indBase2->execute();
+
+    std::string cmdOne = "echo";
+    char *charOne = const_cast<char*>(cmdOne.c_str());
+    std::string argOne = "Hello World Two";
+    char *charTwo = const_cast<char*>(argOne.c_str());
+    char *charAr[3] = {charOne,charTwo,NULL};
+    base *andBase2 = new andConnector(indBase2, charAr);
+    andBase2->execute();
+
+    EXPECT_EQ(andBase2->getSucceeded(),false);
+}
+
+
+TEST(andConnectorTests, using_echo_succeeded_allConnectors) {
+    std::string cmdOneOr = "echo";
+    char *charOneOr = const_cast<char*>(cmdOneOr.c_str());
+    std::string argOneOr = "Hello World";
+    char *charTwoOr = const_cast<char*>(argOneOr.c_str());
+    char *charArOr[3] = {charOneOr,charTwoOr,NULL};
+    base *indBase = new indConnector(charArOr);
+    indBase->execute();
+
+    std::string cmdOne = "echo";
+    char *charOne = const_cast<char*>(cmdOne.c_str());
+    std::string argOne = "Hello World Two";
+    char *charTwo = const_cast<char*>(argOne.c_str());
+    char *charAr[3] = {charOne,charTwo,NULL};
+    base *orBase = new orConnector(indBase, charAr);
+    orBase->execute();
+
+    std::string cmdOneAnd = "echo";
+    char *charOneAnd = const_cast<char*>(cmdOneAnd.c_str());
+    std::string argOneAnd = "Hello World Three";
+    char *charTwoAnd = const_cast<char*>(argOneAnd.c_str());
+    char *charArAnd[3] = {charOneAnd,charTwoAnd,NULL};
+    base *andBase = new andConnector(orBase, charArAnd);
+    andBase->execute();
+
+    EXPECT_EQ(andBase->getSucceeded(), false);
+}
+
+TEST(andConnectorTests, using_touch) {
+   std::string cmdOneOr = "touch";
+   char *charOneOr = const_cast<char*>(cmdOneOr.c_str());
+   std::string argOneOr = "Hello.txt";
+   char *charTwoOr = const_cast<char*>(argOneOr.c_str());
+   char *charArOr[3] = {charOneOr,charTwoOr,NULL};
+   base *indBase = new indConnector(charArOr);
+   indBase->execute();
+
+   std::string cmdOne = "touch";
+   char *charOne = const_cast<char*>(cmdOne.c_str());
+   std::string argOne = "Hello2.txt";
+   char *charTwo = const_cast<char*>(argOne.c_str());
+   char *charAr[3] = {charOne,charTwo,NULL};
+   base *andBase = new andConnector(indBase, charAr);
+   andBase->execute();
+
+   EXPECT_EQ(andBase->getSucceeded(), true);
+}
+
+TEST(andConnectorTests, using_ls) {
+   std::string cmdOneOr = "ls";
+   char *charOneOr = const_cast<char*>(cmdOneOr.c_str());
+   char *charArOr[2] = {charOneOr,NULL};
+   base *indBase = new indConnector(charArOr);
+   indBase->execute();
+
+   std::string cmdOne = "ls";
+   char *charOne = const_cast<char*>(cmdOne.c_str());
+   char *charAr[2] = {charOne,NULL};
+   base *andBase = new andConnector(indBase, charAr);
+   andBase->execute();
+
+   EXPECT_EQ(andBase->getSucceeded(), true);
+}
+
+#endif
