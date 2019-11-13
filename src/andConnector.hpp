35b9a2be12844d6d2de2434d185a6aa5982e8e0b
Sinzunza93 <56137371+Sinzunza93@users.noreply.github.com>
2019-11-13 12:25:55 -0800

    Update andConnector.hpp
    Fixed forking failure when execvp failed

--- src/andConnector.hpp
+++ src/andConnector.hpp
@@ -34,9 +34,14 @@ class andConnector : public base
 				{
 					int status;
 					waitpid(0, &status, WCONTINUED);
-					perror("wait for child failed");
-					if(WIFEXITED(status)){ succeeded = true; }
-				}
+				 	if(status == 0){
+                                        	succeeded = true;
+                                	}
+                        	}
+                        	if(pid == 0){
+                        		_exit(-1);
+                        	}
+
 			}	
 		}		
 };
