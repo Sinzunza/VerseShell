3368d806fc78b99ed6ca6d3f0a1df8e1bfa9778b
Sinzunza93 <56137371+Sinzunza93@users.noreply.github.com>
2019-11-28 16:08:36 -0800

    Create andParen.h

---
+++ headers/andParen.h
@@ -0,0 +1,23 @@
+#ifndef ANDPAREN_H
+#define ANDPAREN_H
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <errno.h>
+
+#include "base.hpp"
+#include "parse.h"
+
+class parse;
+
+class andParen : public base {
+        public:
+            andParen(base* leftBase, std::string command, int &iterator, int &lastIndex);
+            virtual void execute();
+    
+        private:
+            base* leftBase;
+            std::vector <base*> allBases;
+};
+#endif
