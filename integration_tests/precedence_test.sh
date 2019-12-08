3e5f58dda79fbb942912076926f593fcfb300d07
Sinzunza93 <56137371+Sinzunza93@users.noreply.github.com>
2019-12-08 13:53:30 -0800

    Update precedence_test.sh

--- integration_tests/precedence_test.sh
+++ integration_tests/precedence_test.sh
@@ -8,8 +8,8 @@ echo hello ; ( echo a ; echo b)
 echo hello ; ( echo a ; ( echo b ; echo  c ) )
 ( echo A && echo B ) || ( echo C && echo D )
 echo hello && ( echo a ||  ( echo b && echo c ) )
-( echo A && failed B ) || ( echo C && echo D ) && ( echo E || echo G ) 
- echo A && ( ( echo B && echo C ) || ( echo D && echo E ) ) && echo G
+( echo A && failed B ) || ( echo C && echo D ) && ( echo E || echo G )    # output:  A C D E
+ echo A && ( ( echo B && echo C ) || ( echo D && echo E ) ) && echo G     # output:  A B C G
 ( test ../names.txt && [ basse.hpp ] ) || ( exit && echo D )
 exit
 EOL 
