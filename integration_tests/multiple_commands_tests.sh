ab27da281f2028ed159fb1ca101b137e9a1902af
Sinzunza93 <56137371+Sinzunza93@users.noreply.github.com>
2019-12-08 13:30:23 -0800

    Update multiple_commands_tests.sh

--- integration_tests/multiple_commands_tests.sh
+++ integration_tests/multiple_commands_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Beginning single command tests"
+echo "Beginning multiple commands tests"
 echo "Testing commands in order: echo \"Hello World!\" && ls -f, echo \"Hello World!\" || ls -f, badcommand && echo \"Good command!\", badcommand || echo \"Good command!\", echo \"hel\"; echo \"lo world\""
 ../rshell << EOL
 echo "Hello World!" && ls -f
