d0dcfea2fd2a97d839cb2770836d44b93bd1aad9
Sinzunza93 <56137371+Sinzunza93@users.noreply.github.com>
2020-02-10 00:58:49 -0800

    Update exit_command_tests.sh

--- integration_tests/exit_command_tests.sh
+++ integration_tests/exit_command_tests.sh
@@ -2,6 +2,6 @@
 
 echo "Beginning exit command tests"
 echo "Testing command: echo hello || exit; echo world && exit; echo you should not be able to see this"
-../rshell << EOL
+../VerseShell << EOL
 echo hello || exit; echo world && exit; echo you should not be able to see this
 EOL 
