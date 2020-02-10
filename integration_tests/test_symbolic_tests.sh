07cb914f60958c5adf68879bcdb1d7a7d31f460a
Sinzunza93 <56137371+Sinzunza93@users.noreply.github.com>
2020-02-10 01:01:55 -0800

    Update test_symbolic_tests.sh

--- integration_tests/test_symbolic_tests.sh
+++ integration_tests/test_symbolic_tests.sh
@@ -2,7 +2,7 @@
 
 echo "Beginning test symbolic tests"
 echo "Testing commands in order: [ ../names.txt ],[ -e ../src/ ] , [ -f ../CMakeLists.txt ] , [ -d ../names.txt ], [ -d ../images/ ], echo hello && [ -e ../src/ ] || [ ../fail.txt ], [ ../names.txt ] && [ ..fail.txt ] || [ -d ../images/ ], ls || [ -f ../images/ ] && echo hello && [ -e ../names.txt ] " 
-../rshell << EOL
+../VerseShell << EOL
 [ ../names.txt ] 
 [ -e ../src/ ]
 [ -f ../CMakeLists.txt ]
