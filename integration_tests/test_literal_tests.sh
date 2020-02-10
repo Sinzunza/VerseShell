9983dd1c7c97e8ae12abd51d8376417e6df3d8e4
Sinzunza93 <56137371+Sinzunza93@users.noreply.github.com>
2020-02-10 01:01:38 -0800

    Update test_literal_tests.sh

--- integration_tests/test_literal_tests.sh
+++ integration_tests/test_literal_tests.sh
@@ -2,7 +2,7 @@
 
 echo "Beginning test literal tests"
 echo "Testing commands in order: test ../names.txt, test -e ../src/, test -f ../CMakeLists.txt, test -d ../names.txt, test -d ../images/, echo hello && test -e ../src/ || test ../fail.txt, test ../names.txt && test ..fail.txt || test -d ../images/, ls || test -f ../images/ && echo hello && test -e ../names.txt " 
-../rshell << EOL
+../VerseShell << EOL
 test ../names.txt
 test -e ../src/
 test -f ../CMakeLists.txt
