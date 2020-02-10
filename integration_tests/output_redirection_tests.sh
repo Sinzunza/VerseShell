d257e5d8c87c69d84c5a6443abdc8c43fbc6428b
Sinzunza93 <56137371+Sinzunza93@users.noreply.github.com>
2020-02-10 01:00:41 -0800

    Update output_redirection_tests.sh

--- integration_tests/output_redirection_tests.sh
+++ integration_tests/output_redirection_tests.sh
@@ -3,7 +3,7 @@
 echo "Beginning output redirection tests"
 
 echo "Testing single output"
-../rshell << EOL
+../VerseShell << EOL
 touch  aFile.txt
 echo "Hello World" > aFile.txt
 cat aFile.txt
@@ -11,14 +11,14 @@ exit
 EOL
 
 echo "Testing single output override"
-../rshell << EOL 
+../VerseShell << EOL 
 echo "This input should be the first one in the file " > aFile.txt
 cat aFile.txt
 exit
 EOL
 
 echo "Testing single output append"
-../rshell << EOL
+../VerseShell << EOL
 echo "This input should be appended to existing input " >> aFile.txt
 cat aFile.txt
 rm aFile.txt
