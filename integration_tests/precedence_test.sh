b1642527443e54abf7eb782d42767041e772b37b
Sinzunza93 <56137371+Sinzunza93@users.noreply.github.com>
2020-02-10 01:01:02 -0800

    Update precedence_test.sh

--- integration_tests/precedence_test.sh
+++ integration_tests/precedence_test.sh
@@ -3,7 +3,7 @@
 echo "Beginning precedence Testing"
 echo "echo hello ; ( echo a ; echo b) , echo hello ; ( echo a ; ( echo b ; echo  c ) ) , ( echo A && echo B ) || ( echo C && echo D ) , echo hello && ( echo a ||  ( echo b && echo c ) ) ,  ( echo A && failed B ) || ( echo C && echo D ) && ( echo E || echo G ) , echo A && ( ( echo B && echo C ) || ( echo D && echo E ) ) && echo G , ( test a.out && [ basse.hpp ] ) || ( exit && echo D ) "
 
-../rshell << EOL
+../VerseShell << EOL
 echo hello ; ( echo a ; echo b)
 echo hello ; ( echo a ; ( echo b ; echo  c ) )
 ( echo A && echo B ) || ( echo C && echo D )
