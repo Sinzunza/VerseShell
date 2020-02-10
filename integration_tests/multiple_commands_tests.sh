0c9b8a7cf6f68b5d190a3dc0b9328e7d75665b77
Sinzunza93 <56137371+Sinzunza93@users.noreply.github.com>
2020-02-10 00:59:41 -0800

    Update multiple_commands_tests.sh

--- integration_tests/multiple_commands_tests.sh
+++ integration_tests/multiple_commands_tests.sh
@@ -2,7 +2,7 @@
 
 echo "Beginning multiple commands tests"
 echo "Testing commands in order: echo \"Hello World!\" && ls -f, echo \"Hello World!\" || ls -f, badcommand && echo \"Good command!\", badcommand || echo \"Good command!\", echo \"hel\"; echo \"lo world\""
-../rshell << EOL
+../VerseShell << EOL
 echo "Hello World!" && ls -f
 echo "Hello World!" || ls -f
 badcommand && echo "Good command!"
